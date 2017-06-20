14db2d12ec2b728fb5d3953cae221b2d53f77995
Simon Rentzke <simon@rentzke.com>
2017-06-20 15:58:00 +1000

    Init.

---
+++ README.md
@@ -0,0 +1,3 @@
+"Add to Exceed Curation List" is a Chrome extension for Exceed Curators.
+
+For more information visit homepage: http://intellum.com/
---
+++ script.js
@@ -0,0 +1,37 @@
+// onClicked callback function.
+function onClickHandler(info, tab) {
+  if (info.menuItemId == "page") {
+    var postUrl = "http://intellum.exceedlms.com/course_scraped_urls/new?chrome_extension=true&url="+encodeURIComponent(info.pageUrl);
+    chrome.windows.create({"url":postUrl, "type":"popup", "height":350,"width":800, "top": 100, "left": 400, incognito: tab.incognito});
+  }
+  if (info.menuItemId == "link") {
+    var postUrl = "http://intellum.exceedlms.com/course_scraped_urls/new?chrome_extension=true&url="+encodeURIComponent(info.linkUrl);
+    chrome.windows.create({"url":postUrl, "type":"popup", "height":350,"width":800, "top": 100, "left": 400, incognito: tab.incognito});
+  }
+};
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// for toolbar button
+chrome.browserAction.onClicked.addListener(function(tab) {
+  var postUrl = "http://intellum.exceedlms.com/course_scraped_urls/new?chrome_extension=true&url="+encodeURIComponent(tab.url);
+  chrome.windows.create({"url":postUrl, "type":"popup", "height":350,"width":800, "top": 100, "left": 400, incognito: tab.incognito});
+});
+
+
+chrome.runtime.onInstalled.addListener( function(details) {
+
+  // Setting up context menu items.
+  var contexts = ["page","link"];
+  for (var i = 0; i < contexts.length; i++) {
+    var context = contexts[i];
+    var title = "Add " + context + " to Exceed curation list";
+    var id = chrome.contextMenus.create({"title": title, "contexts":[context], "id": context});
+  }
+
+  if(details.reason == "install"){
+    chrome.tabs.create({url: ""});
+  }
+});
+
+chrome.runtime.setUninstallURL("");
